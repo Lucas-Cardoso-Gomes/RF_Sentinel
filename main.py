c11d1a7cbfe743c91cdee34b6cdac844c36247bb
Lucas-Cardoso-Gomes <129914561+Lucas-Cardoso-Gomes@users.noreply.github.com>
2025-07-23 17:48:01 -0300

    0.2 - Sem testes
    ainda necessita testar

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ def toggle_scanner():
 
 if __name__ == "__main__":
     print("Iniciando o servidor web Uvicorn...")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
